45a51ac06d7b107b069b9b4fac8202426c4bc6a9
Vaughan, Jim <jim.vaughan@ingramcontent.com>
2020-08-24 14:04:48 -0500

    Complete chapter 5
    Learned about ownership, borrowing and slices.

---
+++ ownership/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let s1 = String::from("hello world");
+    let word = first_word(&s1);
+    println!("{}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
